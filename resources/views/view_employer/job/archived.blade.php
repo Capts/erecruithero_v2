f62b1f551c889da30dad1d19e20a94c865a20e15
kevinskii <kevin.572.94@gmail.com>
2017-08-13 23:56:51 +0800

    last

--- resources/views/view_employer/job/archived.blade.php
+++ resources/views/view_employer/job/archived.blade.php
@@ -51,7 +51,7 @@
                   </tr>
                   
                
-
+{{-- {{-- {{-- {{-- {{-- {{-- {{-- {{-- asfasf afas --}}sasadaaaaaaaaaaaaaaaaa --}} --}} --}} --}} --}} --}} --}}
                 
                 @endforeach
               </tbody>
