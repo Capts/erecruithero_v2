56b37b0b33432cbbb4fe5863dcb66f8daf4098ea
Capts <kevin.572.94@gmail.com>
2017-08-14 17:58:56 +0800

    yeah

--- database/seeds/SkillTableSeeder.php
+++ database/seeds/SkillTableSeeder.php
@@ -12,16 +12,41 @@ class SkillTableSeeder extends Seeder
      */
     public function run()
     {
+        $employer_skills = new Skill();
+        $employer_skills->user_id = 1;
+        $employer_skills->skill_name = 'Drawing';
+        $employer_skills->save();
+
+
         $applicant_skills = new Skill();
-        $applicant_skills->user_id = 1;
-        $applicant_skills->skill_name = 'Drawing';
+        $applicant_skills->user_id = 2;
+        $applicant_skills->skill_name = 'Programming';
         $applicant_skills->save();
 
+        $app_skills1 = new Skill();
+        $app_skills1->user_id = 3;
+        $app_skills1->skill_name = 'Programming';
+        $app_skills1->save();
 
-        $employer_skills = new Skill();
-        $employer_skills->user_id = 2;
-        $employer_skills->skill_name = 'Painting';
-        $employer_skills->save();
+        $app_skills2 = new Skill();
+        $app_skills2->user_id = 4;
+        $app_skills2->skill_name = 'Programming';
+        $app_skills2->save();
+
+        $applicant_skills3 = new Skill();
+        $applicant_skills3->user_id = 2;
+        $applicant_skills3->skill_name = 'Dancing';
+        $applicant_skills3->save();
+
+        $app_skills4 = new Skill();
+        $app_skills4->user_id = 3;
+        $app_skills4->skill_name = 'Dancing';
+        $app_skills4->save();
+
+        $app_skills5 = new Skill();
+        $app_skills5->user_id = 4;
+        $app_skills5->skill_name = 'Dancing';
+        $app_skills5->save();
 
         
     }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -41,6 +41,28 @@ class UserTableSeeder extends Seeder
         $applicant->save();
         $applicant->roles()->attach($role_applicant);
 
+        $applicant1 = new User();
+        $applicant1->firstname = 'Raymund';
+        $applicant1->lastname = 'Dilan';
+        $applicant1->email = 'raymund@gmail.com';
+        $applicant1->password = bcrypt('secret');
+        $applicant1->avatar = 'public/default/avatars/male.png';
+        $applicant1->slug = str_slug('raymund-dilan');
+        $applicant1->gender = "male";
+        $applicant1->save();
+        $applicant1->roles()->attach($role_applicant);
+
+        $applicant2 = new User();
+        $applicant2->firstname = 'Jeff';
+        $applicant2->lastname = 'Ingosan';
+        $applicant2->email = 'jeff@gmail.com';
+        $applicant2->password = bcrypt('secret');
+        $applicant2->avatar = 'public/default/avatars/male.png';
+        $applicant2->slug = str_slug('jeff-ingosan');
+        $applicant2->gender = "male";
+        $applicant2->save();
+        $applicant2->roles()->attach($role_applicant);
+
 
     }
 
