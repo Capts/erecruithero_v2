7316fb06b6890070ab6ff8770e463a4b58ebbd04
Capts <kevin.572.94@gmail.com>
2017-08-23 07:16:53 +0800

    hell of a commit

---
+++ storage/framework/views/43e04877f3040c27f138ca6e37f5f436a194b26f.php
@@ -0,0 +1,44 @@
+<div class="row">
+	<div class="col-md-12">
+		<!-- Modal for tertiary-->
+
+			<!-- Modal -->
+			<div class="modal fade" id="readmore1" tabindex="1" role="dialog" aria-labelledby="myModalLabel">
+			  <div class="modal-dialog modal-xs" role="document">
+			    <div class="modal-content">
+			     
+			      <div class="modal-body">
+			      	 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			      	<div class="row">
+						
+						<div class="">
+							<div class="row">
+									<div class="col-md-10 col-md-offset-1">
+										<h2 class="summary-heading text-left" ><?php echo e($thisUser->firstname . ' '  . $thisUser->lastname); ?><br><small><?php echo e($thisUser->email); ?></small></h2>
+										<img class="summary-image img-circle img-thumbnail img-responsive pull-left" src="<?php echo e(Storage::url($thisUser->avatar)); ?>" height="200px" width="200px" style="margin-right:20px;"></img>
+										
+										<h3><?php echo e(ucfirst($thisUser->profile->bio)); ?></h3>
+										
+									<div class="box-footer">
+										<button class="btn btn-flat btn-primary btn-block"><i class="fa fa-calendar pull-left">&nbsp;</i>Invite for interview</button>
+									</div>
+									</div>
+								
+							</div>
+							
+						</div>
+							
+						
+
+			      	</div>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+
+		<!--End Modal-->
+
+
+
+	</div>
+</div>
---
+++ storage/framework/views/aec63433227d5e0b78544ba25b7244262a761c6a.php
@@ -0,0 +1,57 @@
+<div class="row">
+	<div class="col-md-12">
+		<!-- Modal for tertiary-->
+			
+			<?php if(is_null($getApply)): ?>
+
+			<?php else: ?>
+
+				<!-- Modal -->
+				<div class="modal fade" id="areyousure1-<?php echo e($getApply->id); ?>" tabindex="1" role="dialog" aria-labelledby="myModalLabel">
+				  <div class="modal-dialog modal-xs" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        
+				        <h4 class="modal-title" id="myModalLabel"><i class="fa fa-question-circle fa-2x pull-right" style="color:#2e7489;"></i> &nbsp;Cancel application?</h4>
+				      </div>
+				      <div class="modal-body">
+				      	<div class="row">
+							
+							<div class="text-center">
+								<div class="row">
+									<div class="btn-group" style="width: 90%; margin:20px 0px;">
+										
+														
+														
+													<?php echo Form::open(['route'=> ['apply.destroy', $getApply->id], 'method' => 'POST']); ?>
+
+														<?php echo e(method_field('delete')); ?>
+
+												    	<?php echo Form::submit('cancel application', ["class" => 'btn btn-flat btn-danger pull-left']); ?>
+
+
+										    		<?php echo Form::close(); ?>
+
+									</div>
+										
+									
+								</div>
+								
+							</div>
+								
+							
+
+				      	</div>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+			<?php endif; ?>
+			
+
+		<!--End Modal-->
+
+
+
+	</div>
+</div>
